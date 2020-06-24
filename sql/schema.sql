342dc14e9c1712d3acf559cc536884809833f9b6
AMA <alexanderabram6440@gmail.com>
2020-06-24 17:14:41 -0500

    created department table in schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,3 +1,9 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 USE employees_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
+);
