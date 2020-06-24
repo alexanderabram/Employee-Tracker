ff813ed3078469268a6d7f103ea7d1284d680754
AMA <alexanderabram6440@gmail.com>
2020-06-24 18:49:09 -0500

    wrote view all employees function and view department function

--- index.js
+++ index.js
@@ -9,81 +9,116 @@ var connection = mysql.createConnection({
     database: "employees_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     employeeTrack(); //NEED TO RUN ACTUAL FUNCTION
 });
 
-function employeeTrack () {
+function run() {
     inquirer
-    .prompt({
-        name: "start",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-            "View ALL Employees",
-            "View ALL Employees by Department",
-            "View ALL Employees by Manager",
-            "Add Employee",
-            "Remove Employee",
-            "Add Department",
-            "Remove Department",
-            "Add Role",
-            "Remove Role",
-            "Update Employee Role",
-            "Update Employee Manager",
-            "Exit"
-        ]
-    })
-    .then(function (res) {
-        switch (res.employeeTrack){
-            case "View ALL Employees":
-            viewEm();
-            break;
+        .prompt({
+            name: "start",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View ALL Employees",
+                "View ALL Employees by Department",
+                "View ALL Employees by Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Add Department",
+                "Remove Department",
+                "Add Role",
+                "Remove Role",
+                "Update Employee Role",
+                "Update Employee Manager",
+                "Exit"
+            ]
+        })
+        .then(function (res) {
+            switch (res.employeeTrack) {
+                case "View ALL Employees":
+                    viewEm();
+                    break;
 
-            case "View ALL Employees by Department":
-            viewEmByD();
-            break;
+                case "View ALL Employees by Department":
+                    viewEmByD();
+                    break;
 
-            case "View ALL Employees by Manager":
-                viewEmByMa();
-                break;
-            
-            case "Add Employee":
-                addEm ();
-                break;
+                case "View ALL Employees by Manager":
+                    viewEmByMa();
+                    break;
 
-            case "Remove Employee":
-                remEm ();
-                break;
+                case "Add Employee":
+                    addEm();
+                    break;
 
-            case "Add Department":
-                addDep();
-                break;
-            
-            case "Remove Department":
-                remDep();
-                break;
+                case "Remove Employee":
+                    remEm();
+                    break;
 
-            case "Add Role":
-                addRole();
-                break;
+                case "Add Department":
+                    addDep();
+                    break;
 
-            case "Remove Role":
-                remRole();
-                break;
+                case "Remove Department":
+                    remDep();
+                    break;
 
-            case "Update Employee Role":
-                updateEmRole();
-                break;
+                case "Add Role":
+                    addRole();
+                    break;
 
-            case "Update Employee Manager":
-                updateEmMa();
-                break;
+                case "Remove Role":
+                    remRole();
+                    break;
 
-            case "Exit":
-                connection.end();
-                break;
-        }
+                case "Update Employee Role":
+                    updateEmRole();
+                    break;
+
+                case "Update Employee Manager":
+                    updateEmMa();
+                    break;
+
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        })
+}
+
+function viewEm() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS \"role\", manager.first_name AS \"manager\" FROM employee LEFT JOIN role ON employee.role_id = roles.id LEFT JOIN employee manager ON employee.manager_id = manager.id GROUP BY employee.id",
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            run();
+        });
+}
+
+
+function viewEmByD(){
+    connection.query ("SELECT * FROM department", function(err,res) {
+        console.table(res);
+        run();
     })
 }
+
+function viewEmByMa()
+
+function addEm()
+
+function remEm()
+
+function addDep()
+
+function remDep()
+
+function addRole()
+
+function remRole()
+
+function updateEmRole()
+
+function updateEmMa()
